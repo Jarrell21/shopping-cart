0a7d4cdcca9b8c2d6f26331fd930a67b5865e789
Jarrell Mikko Romero <jarrellmikkoromero@gmail.com>
2024-01-04 20:03:22 +0800

    Remove counter

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,10 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit"
-import counterReducer from "./counter/counterSlice"
 import { apiSlice } from "./api/apiSlice"
 import cartReducer from "./cart/cartSlice"
 import orderReducer from "./orders/orderSlice"
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     cart: cartReducer,
     orders: orderReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
